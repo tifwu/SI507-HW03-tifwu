4f04b5f8a193bcc11969b656371db134b16e8b26
ShirleyHsu <35506841+shirley1206@users.noreply.github.com>
2018-09-18 16:50:19 -0400

    Update magic_eight.py

--- magic_eight.py
+++ magic_eight.py
@@ -1,6 +1,8 @@
 from random import randint
 
 def magic_eight():
+	answerlist=[" “It is certain.”, “It is decidedly so.”, “Without a doubt.”, “Yes - definitely.”, “You may rely on it.”,  “As I see it, yes.”, “Most likely.”, “Outlook good.”, “Yes.”, “Signs point to yes.”, “Reply hazy, try again”, “Ask again later.”, “Better not tell you now.”, “Cannot predict now.”, “Concentrate and ask again.”, “Don't count on it.”, “My reply is no.”,  “My sources say no.”, “Outlook not so good.”, “Very doubtful.”]
+
 	while True:
 		answer = input('What is your question?')
 		if answer == "quit":
@@ -11,7 +13,6 @@ def magic_eight():
 		else:
 			answer = answerlist[randint(0, len(answerlist))]	
 
-	answerlist=[" “It is certain.”, “It is decidedly so.”, “Without a doubt.”, “Yes - definitely.”, “You may rely on it.”,  “As I see it, yes.”, “Most likely.”, “Outlook good.”, “Yes.”, “Signs point to yes.”, “Reply hazy, try again”, “Ask again later.”, “Better not tell you now.”, “Cannot predict now.”, “Concentrate and ask again.”, “Don't count on it.”, “My reply is no.”,  “My sources say no.”, “Outlook not so good.”, “Very doubtful.”]
 	
 	return answer
 
