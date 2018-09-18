241672ed081e2db545b11ff5a9c7d07c79a8c2fa
ShirleyHsu <35506841+shirley1206@users.noreply.github.com>
2018-09-18 17:25:55 -0400

    Update magic_eight.py

--- magic_eight.py
+++ magic_eight.py
@@ -1,5 +1,4 @@
 from random import randint
-
 def magic_eight():
 	answerlist=['It is certain.', 'It is decidedly so.', 'Without a doubt.', 'Yes - definitely.', 'You may rely on it.',  'As I see it, yes.', 'Most likely.', 'Outlook good.', 'Yes.', 'Signs point to yes.', 'Reply hazy, try again', 'Ask again later.','Better not tell you now.', 'Cannot predict now.', 'Concentrate and ask again.', 'Don\'t count on it.', 'My reply is no.',  'My sources say no.', 'Outlook not so good.', 'Very doubtful.']
 
@@ -8,12 +7,12 @@ def magic_eight():
 		if answer == "quit":
 			break
 		elif answer[-1] != "?":
-			answer = 'I am sorry, I can only answer questions.'
+			print('I am sorry, I can only answer questions.')
 		
 		else:
-			answer = answerlist[randint(0, len(answerlist))]	
-
+			final = answerlist[randint(0, len(answerlist))]	
+			print(final)
 	
-	return answer
 
+magic_eight()
 
