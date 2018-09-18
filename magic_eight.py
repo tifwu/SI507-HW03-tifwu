f90800155e2ff0f749dcfb4f99ff2a37f68cbb9b
Shirley <shirleey@umich.edu>
2018-09-18 17:04:19 -0400

    final edit

--- magic_eight.py
+++ magic_eight.py
@@ -1,14 +1,14 @@
 from random import randint
 
 def magic_eight():
-	answerlist=[" “It is certain.”, “It is decidedly so.”, “Without a doubt.”, “Yes - definitely.”, “You may rely on it.”,  “As I see it, yes.”, “Most likely.”, “Outlook good.”, “Yes.”, “Signs point to yes.”, “Reply hazy, try again”, “Ask again later.”, “Better not tell you now.”, “Cannot predict now.”, “Concentrate and ask again.”, “Don't count on it.”, “My reply is no.”,  “My sources say no.”, “Outlook not so good.”, “Very doubtful.”]
+	answerlist=['It is certain.', 'It is decidedly so.', 'Without a doubt.', 'Yes - definitely.', 'You may rely on it.',  'As I see it, yes.', 'Most likely.', 'Outlook good.', 'Yes.', 'Signs point to yes.', 'Reply hazy, try again', 'Ask again later.','Better not tell you now.', 'Cannot predict now.', 'Concentrate and ask again.', 'Don\'t count on it.', 'My reply is no.',  'My sources say no.', 'Outlook not so good.', 'Very doubtful.']
 
 	while True:
 		answer = input('What is your question?')
 		if answer == "quit":
 			break
-		elif if answer[-1] != "?":
-			answer = "I’m sorry, I can only answer questions."
+		elif answer[-1] != "?":
+			answer = 'I am sorry, I can only answer questions.'
 		
 		else:
 			answer = answerlist[randint(0, len(answerlist))]	
